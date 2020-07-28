3adfb8864979d18b43d23ab72111b8ea6d897fb8
mayankrd <ranjandayal.m@northeastern.edu>
2020-07-28 11:40:50 -0400

    added navbar compoenent

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Navbar from './Components/Layout/Navbar';
 
 function App() {
   return (
     <div className="App">
-     <h1> Hello ! </h1>
+     <Navbar></Navbar>
     </div>
   );
 }
